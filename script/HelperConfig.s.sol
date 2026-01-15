843e0295e31495c59513467b21c8755a31dd2684
Joel Villena <villenajv43@gmail.com>
2026-01-15 16:52:49 +0800

    added more functions

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,11 +3,14 @@
 pragma solidity 0.8.31;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     //
 
     NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         address priceFeed;
@@ -25,20 +28,28 @@ contract HelperConfig is Script {
         pure
         returns (NetworkConfig memory)
     {
-        return
-            NetworkConfig({
-                priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-            });
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+
+        return sepoliaConfig;
     }
 
-    function getDefaultEthConfig()
-        internal
-        pure
-        returns (NetworkConfig memory)
-    {
-        return
-            NetworkConfig({
-                priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-            });
+    function getDefaultEthConfig() internal returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory defaultConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return defaultConfig;
     }
 }
--- src/FundMe.sol
+++ src/FundMe.sol
@@ -6,44 +6,63 @@ import {PriceConverter} from "./PriceConverter.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 error NotOwner();
+error WithdrawFailed();
 
 contract FundMe {
     using PriceConverter for uint256;
 
     uint256 public constant MINIMUM_USD = 5e18;
-    address[] public funders;
-    mapping(address funders => uint256 amount) public listOfFunders;
-    address public immutable I_OWNER;
-    AggregatorV3Interface priceFeed;
+    address[] public s_funders;
+    mapping(address funders => uint256 amount) public s_listOfFunders;
+    address private immutable I_OWNER;
+    AggregatorV3Interface s_priceFeed;
 
     constructor(address _priceFeed) {
         I_OWNER = msg.sender;
-        priceFeed = AggregatorV3Interface(_priceFeed);
+        s_priceFeed = AggregatorV3Interface(_priceFeed);
     }
 
     function fund() public payable {
         require(
-            msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
             "Didn't send enough ETH"
         );
-        funders.push(msg.sender);
-        listOfFunders[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+        s_listOfFunders[msg.sender] += msg.value;
     }
 
     function getVersion() public view returns (uint256) {
-        return priceFeed.version();
+        return s_priceFeed.version();
+    }
+
+    function cheaperWithdraw() external onlyOwner {
+        address[] memory m_funders = s_funders;
+        uint256 length = m_funders.length;
+
+        for (uint256 i = 0; i < length; ) {
+            s_listOfFunders[m_funders[i]] = 0;
+            unchecked {
+                ++i;
+            }
+        }
+
+        delete s_funders;
+
+        address owner = msg.sender;
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        if (!success) revert WithdrawFailed();
     }
 
     function withdraw() public onlyOwner {
         for (
             uint256 funderIndex = 0;
-            funderIndex < funders.length;
+            funderIndex < s_funders.length;
             funderIndex++
         ) {
-            address funder = funders[funderIndex];
-            listOfFunders[funder] = 0;
+            address funder = s_funders[funderIndex];
+            s_listOfFunders[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
 
         // transfer
         // payable(msg.sender).transfer(address(this).balance);
@@ -74,5 +93,19 @@ contract FundMe {
 
     fallback() external payable {
         fund();
+    }
+
+    function getAddressToAmountFunded(
+        address funder
+    ) external view returns (uint256) {
+        return s_listOfFunders[funder];
+    }
+
+    function getFunder(uint256 index) external view returns (address) {
+        return s_funders[index];
+    }
+
+    function getOwner() external view returns (address) {
+        return I_OWNER;
     }
 }
